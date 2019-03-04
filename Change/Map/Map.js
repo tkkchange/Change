1900b9d05dc72e21625a70ab4b622a79654300d6
mationleo <34881794+mationleo@users.noreply.github.com>
2019-03-04 19:07:58 +0800

    地图定位
    地图定位

--- Change/Map/Map.js
+++ Change/Map/Map.js
@@ -1,17 +1,62 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    longitude: 118.0462503433,
+    latitude: 24.3669571927,
+    speed: 0,
+    accuracy: 0
   },
+  //事件处理函数
+  bindViewTap: function () {
 
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var that = this
+    wx.showLoading({
+      title: "定位中",
+      mask: true
+    })
+    wx.getLocation({
+      type: 'gcj02',
+      altitude: true,//高精度定位
+      //定位成功，更新定位结果
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        var speed = res.speed
+        var accuracy = res.accuracy
+
+        that.setData({
+          longitude: longitude,
+          latitude: latitude,
+          speed: speed,
+          accuracy: accuracy
+        })
+      },
+      //定位失败回调
+      fail: function () {
+        wx.showToast({
+          title: "定位失败",
+          icon: "none"
+        })
+      },
+
+      complete: function () {
+        //隐藏定位中信息进度
+        wx.hideLoading()
+      }
+
+    })
   },
 
   /**
