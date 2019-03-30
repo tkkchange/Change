784058972c68484750b7d07af1524e862e4c2d2b
mationleo <34881794+mationleo@users.noreply.github.com>
2019-03-31 01:23:59 +0800

    商家页面-店铺环境图片栏

--- Change/DetailItemList/DetailItemList.js
+++ Change/DetailItemList/DetailItemList.js
@@ -11,7 +11,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options);
+    /**
+     * 动态传参设置标题
+     */
     wx.setNavigationBarTitle({
       title: options.name
     })
