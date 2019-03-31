0da899159a0006a92fa23d0d0152fe20e2f06730
mationleo <34881794+mationleo@users.noreply.github.com>
2019-03-31 14:54:23 +0800

    领取按钮

--- Change/DetailShopList/DetailShopList.js
+++ Change/DetailShopList/DetailShopList.js
@@ -11,7 +11,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    /**
+     * 动态传参设置标题
+     */
+    wx.setNavigationBarTitle({
+      title: options.name
+    })
   },
 
   /**
