4f873eb2850db7ff82da843fbc2e47a50b389041
mationleo <34881794+mationleo@users.noreply.github.com>
2019-03-31 20:17:47 +0800

    店铺列表

--- Change/DetailShopList/DetailShopList.js
+++ Change/DetailShopList/DetailShopList.js
@@ -4,7 +4,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    /*课程信息组件*/
+    bsnsInfo: [
+      {
+        img: "../../../images/icon/first.png",
+        name: "商家1",
+        ads: "福建漳州招商局经济技术开发区厦门大学漳州校区",
+        grade: "1",
+        link: '../DetailShopList/DetailShopList'
+      }, {
+        img: "../../../images/icon/first.png",
+        name: "商家2",
+        ads: "福建漳州招商局经济技术开发区厦门大学漳州校区",
+        grade: "2",
+        link: '../DetailShopList/DetailShopList'
+      }, {
+        img: "../../../images/icon/first.png",
+        name: "商家3",
+        ads: "福建漳州招商局经济技术开发区厦门大学漳州校区",
+        grade: "3",
+        link: '../DetailShopList/DetailShopList'
+      }, {
+        img: "../../../images/icon/first.png",
+        name: "商家4",
+        ads: "福建漳州招商局经济技术开发区厦门大学漳州校区",
+        grade: "4",
+        link: "../DetailShopList/DetailShopList"
+      }, {
+        img: "../../../images/icon/first.png",
+        name: "商家5",
+        ads: "福建漳州招商局经济技术开发区厦门大学漳州校区",
+        grade: "5",
+        link: '../DetailShopList/DetailShopList'
+      }
+    ]
   },
 
   /**
