48e786119683080dc0d36d410847c6d63bf8d596
ScooperCC <1015833266@qq.com>
2019-07-22 12:10:08 +0800

    小程序json示范

--- www/ShopList.php
+++ www/ShopList.php
@@ -6,21 +6,22 @@ $password = "ec_psd";
 $dbname = "echange";
 //所需数据
 $name=$_REQUEST["SK_name"];
-$intro=$_REQUEST["SK_intro"];
+$adress=$_REQUEST["SK_adress"];
 $pic=$_REQUEST["SK_pic"];
+$grade=$_REQUEST["SK_grade"];
 //链接数据库
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
 
-$sql = "SELECT SK_name,SK_intro,SK_pic FROM shopkeeper";
+$sql = "SELECT SK_name,SK_adress,SK_pic,SK_grade FROM shopkeeper";
 $result = $conn->query($sql);
  
 if ($result->num_rows > 0) {
     // 输出数据
     while($row = $result->fetch_assoc()) {
-        echo $row["SK_name"].$row["SK_intro"].$row["SK_pic"];
+        echo $row["SK_name"].$row["SK_adress"].$row["SK_pic"].$row["SK_grade"];
     }
 } else {
     echo "无数据";
